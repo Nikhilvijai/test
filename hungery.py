b90aa0ce196b0af1f8b064e16499bdb0482f7753
nikhil <nikhil@example.com>
2025-12-05 21:44:25 +0530

    added an extra in thirsty

--- hungery.py
+++ hungery.py
@@ -5,6 +5,9 @@ if hungary == "yes":
     print("eat samosa")
     print("eat burger")
 else:
-    print("im not")
-    print('go and study')
+    thirsty = input("are you thirsty:")
+    if thirsty == "yes":
+        print("im thirsty")
+    else:
+        print('go and study')
     
